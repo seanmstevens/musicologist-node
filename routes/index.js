adcdf41dbfc1c2ec0670400d834891693bc54787
Sean Stevens <sstevens88@gmail.com>
2017-09-02 18:45:45 -0700

    Added allowed methods to initial get response

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Methods", "GET, POST");
   res.render('index', { title: 'Musicologist' });
 });
 
